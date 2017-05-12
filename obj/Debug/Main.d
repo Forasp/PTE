4101e24e98911ecf737619c50ebd2eda08f7e319
timpo88m <timpo88m@gmail.com>
2017-05-12 18:58:25 -0500

    Forgot to clean the build before submitting! - Derp.

--- obj/Debug/Main.d
+++
@@ -1,9 +0,0 @@
-obj/Debug/Main.o: Source/Main.cpp Source/../Include/Main.h \
- Source/../Include/../Include/Object.h \
- Source/../Include/../Include/GlobalDefines.h
-
-Source/../Include/Main.h:
-
-Source/../Include/../Include/Object.h:
-
-Source/../Include/../Include/GlobalDefines.h:
--- obj/Debug/Object.d
+++
@@ -1,6 +0,0 @@
-obj/Debug/Object.o: Source/Object.cpp Source/../Include/Object.h \
- Source/../Include/GlobalDefines.h
-
-Source/../Include/Object.h:
-
-Source/../Include/GlobalDefines.h:
